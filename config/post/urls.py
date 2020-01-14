fffcca780881e748548e7d2b12681d9522c4bbc4
dolgovaar <dolgova.ad@phystech.edu>
2020-01-15 01:03:53 +0300

    added files

--- config/post/urls.py
+++ config/post/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import hello, index, hello1, login, add, redirect_to_index
+from .views import index, login, add, redirect_to_index, lists
 
 urlpatterns = [
     path('', redirect_to_index),
-    path('hello/', hello, name="hello"),
-    path('hello1/<int:sss>', hello1, name="hello1"),
     path('index/', index, name="index"),
     path('login/', login, name="login"),
     path('add/', add, name="add"),
+    path('lists/', lists, name="lists"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
