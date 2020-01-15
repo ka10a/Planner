67650e9d4653df058108e3c173f687e4e5c60d57
imroggen <ka10anovo@gmail.com>
2020-01-15 14:32:59 +0300

    add to do list

--- config/post/models.py
+++ config/post/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 
 
-from django.db import models
-
 class Event(models.Model):
     date = models.DateTimeField('event date')
     name = models.CharField(max_length=200)
 
-    def str(self):
+    def __str__(self):
         return self.name
+
 
 class Calendar(models.Model):
     name = models.CharField(max_length=200)
 
-    def str(self):
+    def __str__(self):
+        return self.name
+
+
+class ToDoItem(models.Model):
+    name = models.CharField(max_length=200)
+    todo = models.CharField(max_length=1000)
+    date = models.DateField('task date')
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
         return self.name
