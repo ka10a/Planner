252b04b7843e30882cdbbf47cf6a25b34ab7f039
dolgovaar <dolgova.ad@phystech.edu>
2020-01-16 07:25:58 +0300

    final 2.2

--- config/config/settings.py
+++ config/config/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = 'calendar'
+LOGIN_REDIRECT_URL = 'lists'
 LOGOUT_REDIRECT_URL = 'index'
 
 CORS_ORIGIN_WHITELIST = [
